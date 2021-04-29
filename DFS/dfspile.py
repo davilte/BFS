15baccc35a4adcaa90c901be9ecea900c07d1950
JonasTB <jonastimbauba7@hotmail.com>
2021-04-28 21:00:36 -0300

    add comments

--- DFS/dfspile.py
+++ DFS/dfspile.py
@@ -15,19 +15,29 @@ adj_list = {
 }
 a = [(8, 3), (3, 1), (3, 2), (3, 4), (1, 4), (2, 4), (4, 7), (4, 6), (6, 7), (6, 5), (7, 5), (5, 0)]
 
+# funcao com grafo e o no principal.
 def dfs(graph, node):
+    #adicionando o node que esta visitando e adicionando ele na pilha.
     visited = [node]
     stack = [node]
+
     while stack:
+        #enquanto nao for vazia, continua.
         node = stack[-1]
+        #adiciona o no visitado.
         if node not in visited:
             visited.append(node)
         remove_from_stack = True
+        #passa o proximo a ser visitado.
         for next in graph[node]:
             if next not in visited:
+                #adiciona o novo na pilha.
                 stack.append(next)
                 remove_from_stack = False
+                #enquanto nao acabar os proximos, ela retorna na condicao.
                 break
+
+        #caso acabe os proximos, irar voltar e remover os visitados.    
         if remove_from_stack:
             stack.pop()
     return visited
